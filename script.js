df8241c34e56c1854e16cc8fd66ec33255605269
tanmaylagoo <tanmaylagoo@gmail.com>
2025-08-12 08:46:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded",()=>{
     const humidity=document.getElementById("humidity");
     const wind=document.getElementById("wind");
     const error_message=document.getElementById("error-message");
-    const apikey="6b7a43dc2ac651875f9e0b15b49ca1fa"
+    const apikey="" //api key hidden on purpose still learning how to use an environment to hide API key
 
     getweatherbtn.addEventListener('click', async()=>{
         const city=cityinput.value.trim()
@@ -64,4 +64,5 @@ document.addEventListener("DOMContentLoaded",()=>{
 
 
     
+
   
